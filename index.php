25bd152261e2bf4be037d8c873d23e8a31f56e9d
Ryan Karpeles <rkarpel@jmls.edu>
2013-10-22 13:21:06 -0500

    New colors and fonts

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html class="no-js">
+	<head>
+	  <meta charset="utf-8">
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	  <title>The Office of Alumni Relations &amp; Development | The John Marshall Law School</title>
+	  <meta name="description" content="">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Stylesheets -->
+		<link rel="stylesheet" href="css/main.css" media="screen" type="text/css">
+		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic" type="text/css">
+
+	</head>
+	<body>
+		<h1>Headline One</h1>
+		<p>Never in all their history <a href="" class="bold">have men</a> been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.</p>
+		<h2>Second Headline</h2>
+		<p>As I stand out here in the wonders of the unknown at Hadley, I sort of realize there’s a fundamental truth to our nature, Man must explore . . . and this is exploration at its greatest.</p>
+		<h3>Third Headline</h3>
+		<p>Here men from the planet Earth first set foot upon the Moon. July 1969 AD. We came in peace for all mankind.</p>
+		<h4>The Fourth Headline</h4>
+		<p>An unordered list:</p>
+		<ul>
+	  	<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+	  	<li>Aliquam tincidunt mauris eu risus.</li>
+	  	<li>Vestibulum auctor dapibus neque.</li>
+		</ul>
+		<p>An ordered list:</p>
+		<ol>
+	  	<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+	  	<li>Aliquam tincidunt mauris eu risus.</li>
+	  	<li>Vestibulum auctor dapibus neque.</li>
+		</ol>
+	</body>
+</html>
