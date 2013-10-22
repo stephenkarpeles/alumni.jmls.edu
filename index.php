a503fe81b29fbbcea396f80de77a088313ab918d
Ryan Karpeles <rkarpel@jmls.edu>
2013-10-22 16:39:32 -0500

    Add grid, layout, styles, footer

--- index.php
+++ index.php
@@ -13,24 +13,91 @@
 
 	</head>
 	<body>
-		<h1>Headline One</h1>
-		<p>Never in all their history <a href="" class="bold">have men</a> been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.</p>
-		<h2>Second Headline</h2>
-		<p>As I stand out here in the wonders of the unknown at Hadley, I sort of realize there’s a fundamental truth to our nature, Man must explore . . . and this is exploration at its greatest.</p>
-		<h3>Third Headline</h3>
-		<p>Here men from the planet Earth first set foot upon the Moon. July 1969 AD. We came in peace for all mankind.</p>
-		<h4>The Fourth Headline</h4>
-		<p>An unordered list:</p>
-		<ul>
-	  	<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-	  	<li>Aliquam tincidunt mauris eu risus.</li>
-	  	<li>Vestibulum auctor dapibus neque.</li>
-		</ul>
-		<p>An ordered list:</p>
-		<ol>
-	  	<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-	  	<li>Aliquam tincidunt mauris eu risus.</li>
-	  	<li>Vestibulum auctor dapibus neque.</li>
-		</ol>
+		<div class="container">
+
+			<div class="grid-wrap">
+				<div class="grid-col bp1-col-1-2">
+					<h1>Headline One</h1>
+					<p>Never in all their history <a href="" class="bold">have men</a> been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.</p>
+					<h2>Second Headline</h2>
+					<p>As I stand out here in the wonders of the unknown at Hadley, I sort of realize there’s a fundamental truth to our nature, Man must explore . . . and this is exploration at its greatest.</p>					
+				</div>
+
+				<div class="grid-col bp1-col-1-2">
+					<h1>Headline One</h1>
+					<p>Never in all their history <a href="" class="bold">have men</a> been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.</p>
+					<h2>Second Headline</h2>
+					<p>As I stand out here in the wonders of the unknown at Hadley, I sort of realize there’s a fundamental truth to our nature, Man must explore . . . and this is exploration at its greatest.</p>
+					<h3>Third Headline</h3>
+					<p>Here men from the planet Earth first set foot upon the Moon. July 1969 AD. We came in peace for all mankind.</p>
+				</div>
+			</div><!-- .grid-wrap -->
+
+			<h1>Headline One</h1>
+			<p>Never in all their history <a href="" class="bold">have men</a> been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.</p>
+			<h2>Second Headline</h2>
+			<p>As I stand out here in the wonders of the unknown at Hadley, I sort of realize there’s a fundamental truth to our nature, Man must explore . . . and this is exploration at its greatest.</p>
+			<h3>Third Headline</h3>
+			<p>Here men from the planet Earth first set foot upon the Moon. July 1969 AD. We came in peace for all mankind.</p>
+			<h4>The Fourth Headline</h4>
+			<p>An unordered list:</p>
+			<ul>
+		  	<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+		  	<li>Aliquam tincidunt mauris eu risus.</li>
+		  	<li>Vestibulum auctor dapibus neque.</li>
+			</ul>
+			<p>An ordered list:</p>
+			<ol>
+		  	<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+		  	<li>Aliquam tincidunt mauris eu risus.</li>
+		  	<li>Vestibulum auctor dapibus neque.</li>
+			</ol>
+
+		</div><!-- .container -->
+
+		<div class="footer">
+			<div class="container">
+				<div class="grid-wrap">
+					<div class="grid-col col-1-4">
+						<h4>Get Involved</h4>
+						<ul>
+							<li><a href="">Link</a></li>
+							<li><a href="">Link</a></li>
+							<li><a href="">Link</a></li>
+							<li><a href="">Link</a></li>
+						</ul>
+					</div>
+					<div class="grid-col col-1-4">
+						<h4>Get Involved</h4>
+						<ul>
+							<li><a href="">Link</a></li>
+							<li><a href="">Link</a></li>
+							<li><a href="">Link</a></li>
+							<li><a href="">Link</a></li>
+						</ul>
+					</div>
+					<div class="grid-col col-1-4">
+						<h4>Get Involved</h4>
+						<ul>
+							<li><a href="">Link</a></li>
+							<li><a href="">Link</a></li>
+							<li><a href="">Link</a></li>
+							<li><a href="">Link</a></li>
+						</ul>
+					</div>
+					<div class="grid-col col-1-4">
+						<h4>Get Involved</h4>
+						<ul>
+							<li><a href="">Link</a></li>
+							<li><a href="">Link</a></li>
+							<li><a href="">Link</a></li>
+							<li><a href="">Link</a></li>
+						</ul>
+					</div>
+				</div>
+
+			</div><!-- .container -->
+		</div><!-- .footer -->
+
 	</body>
 </html>
