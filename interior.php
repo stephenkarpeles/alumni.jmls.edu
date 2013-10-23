b389b99ebd146fa185f9b3cab2cfe25db31c2768
Ryan Karpeles <rkarpel@jmls.edu>
2013-10-23 16:45:31 -0500

    Update interior page

--- interior.php
+++ interior.php
@@ -3,12 +3,48 @@
 
 	<div class="grid-wrap">
 		<div class="grid-col col-1-4">
-			
-			
+			<div class="header-bar header-bar-red">Get Involved Menu</div>
+			<ul>
+				<li><a href="">Interior Link</a></li>
+				<li><a href="">Interior Link</a></li>
+				<li><a href="">Interior Link</a></li>
+				<li><a href="">Interior Link</a></li>
+				<li><a href="">Interior Link</a></li>
+				<li><a href="">Interior Link</a></li>
+			</ul>
+
+			<div class="header-bar header-bar-gray">Connect Online</div>
+			<div class="feature-box feature">
+				<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+			</div>
+
+			<div class="header-bar header-bar-gold">Briefcase</div>
+			<div class="feature-box feature-box-gold">
+				<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+			</div>
+
+			<div class="header-bar header-bar-gold">Upcoming Events</div>
+			<div class="feature-box">
+				<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+			</div>			
 		</div>
+
 		<div class="grid-col col-3-4">
-			<h1>Get Involved</h1>
-			<p class="featured-text">As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm, living object looked so fragile.</p>			
+			<h1 class="page-header">Get Involved</h1>
+			<p class="featured-text">As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm, living object looked so fragile.</p>		
+			
+			<div class="grid-wrap">
+				<div class="grid-col col-1-3">
+					<img src="http://placehold.it/220x133" alt="">
+				</div>
+				<div class="grid-col col-1-3">
+					<img src="http://placehold.it/220x133" alt="">
+				</div>
+				<div class="grid-col col-1-3">
+					<img src="http://placehold.it/220x133" alt="">
+				</div>
+			</div><!-- .grid-wrap -->
+
 		</div>
 	</div><!-- .grid-wrap -->
 
