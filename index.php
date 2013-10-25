ec0567b1ea32ef93cb5c1e85af54113a1ef087e4
Ryan Karpeles <rkarpel@jmls.edu>
2013-10-25 13:41:33 -0500

    Update index.php with action bars

--- index.php
+++ index.php
@@ -31,12 +31,9 @@
 			</div>
 
 			<div class="grid-col bp3-col-1-3">
-				<h1>Headline One</h1>
-				<p>Never in all their history <a href="" class="bold">have men</a> been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.</p>
-				<h2>Second Headline</h2>
-				<p>As I stand out here in the wonders of the unknown at Hadley, I sort of realize there’s a fundamental truth to our nature, Man must explore . . . and this is exploration at its greatest.</p>
-				<h3>Third Headline</h3>
-				<p>Here men from the planet Earth first set foot upon the Moon. July 1969 AD. We came in peace for all mankind.</p>
+				<div class="action-bar bold mar-bot-small"><a href="">Get Involved</a></div>
+				<div class="action-bar bold mar-bot-small"><a href="">Stay Connected</a></div>
+				<div class="action-bar bold mar-bot-small"><a href="">Support John Marshall</a></div>
 			</div>
 		</div><!-- .grid-wrap -->
 
