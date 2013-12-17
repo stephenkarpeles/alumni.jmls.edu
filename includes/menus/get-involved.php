ec6a4cd89db8622022634ebba6019d0acacf7c92
Ryan Karpeles <rkarpel@jmls.edu>
2013-12-17 16:45:46 -0600

    Update get involved menu

--- includes/menus/get-involved.php
+++ includes/menus/get-involved.php
@@ -2,11 +2,11 @@
 	<div class="header-bar header-bar-red">Get Involved Menu</div>
 	<div class="sub-nav">
 		<ul class="no-list-style">
-			<li><a href="">Young Alumni Council</a></li>
-			<li><a href="">Alumni Association</a></li>
-			<li><a href="">Mentoring Program</a></li>
-			<li><a href="">Alumni Meetings &amp; Events</a></li>
-			<li><a href="">Volunteer Opportunities</a></li>
+			<li><a href="../get-involved/volunteer-opportunities.php">Volunteer Opportunities</a></li>
+			<li><a href="../get-involved/alumni-events.php">Alumni Events</a></li>
+			<li><a href="../get-involved/alumni-association.php">Alumni Association</a></li>
+			<li><a href="../get-involved/young-alumni-council.php">Young Alumni Council</a></li>
+			<li><a href="../get-involved/get-quoted.php">Get Quoted</a></li>
 		</ul>
 	</div>
 </div>	
