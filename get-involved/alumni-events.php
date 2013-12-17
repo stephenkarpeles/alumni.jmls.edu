12d848d0dda713b448f68107a278f8a87fe4f183
Ryan Karpeles <rkarpel@jmls.edu>
2013-12-17 16:41:47 -0600

    Add content to Alumni Events page

---
+++ get-involved/alumni-events.php
@@ -0,0 +1,41 @@
+<!-- Meta Info -->
+<?php
+// Remove commented out marks before each line you wish to edit, and place content between quote marks.
+// $tpTitle="";
+// $pgDesc="";
+// $pgKeywords="";
+?>
+
+<!-- Header -->
+<?php include('../includes/header.php'); ?>
+
+	<!-- Begin Content -->
+	<section>
+		<div class="grid-wrap">								
+			<div class="grid-col bp3-col-3-4 pull-right-above-bp1">	
+
+				<!-- Main -->			
+				<article class="main">				
+					<h1 class="page-header">Alumni Events</h1>
+					<p class="featured-text">Throughout the year, the Office of Alumni Relations hosts events tailored specifically to the professional needs of our alumni.</p>
+					<h3>CLE Programs</h3>
+					<p>As a service to John Marshall alumni and the legal community, we offer high-quality, affordable CLE and professional development programs. Whether you are brushing up on a practice area or want to learn about something new, you&rsquo;ll be able to meet your CLE requirements with ease. </p>
+					<p>[View a Full List of John Marshall CLE Events] (Link coming)</p>
+					<p><em>Hardship waivers for more recent alumni are available for many CLE programs. Please contact Cliff Scott-Rudnick for fee adjustment requests due to financial hardship at <a href="mailto:9scott@jmls.edu" class="bold">9scott@jmls.edu</a>.</em></p>
+					<h3>Alumni Networking Events &amp; Receptions</h3>
+					<p>Attending alumni receptions allows you a chance to socialize, reminisce, network, and hear about what is going on at your alma mater. Receptions are held throughout the Chicago area and occasionally in various cities around the country.</p>
+					<p>Check back often as we will update this page as events are added and dates are confirmed. As always, make sure that we have your current contact information so that you will receive your invitation.</p>
+				</article><!-- .main -->
+				
+			</div><!-- .grid-col -->			
+						
+			<div class="grid-col bp3-col-1-4">
+				<!-- Sidebar -->
+				<?php include('../includes/sidebar.php'); ?>				
+			</div><!-- .grid-col -->			
+
+		</div><!-- .grid-wrap -->	
+	</section>
+
+<!-- Footer -->
+<?php include('../includes/footer.php'); ?>	
