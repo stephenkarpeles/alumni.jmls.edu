be0fc6d3fdb40380c400c42af16d68dee820f6a2
Ryan Karpeles <rkarpel@jmls.edu>
2013-12-18 11:32:13 -0600

    Update content

---
+++ get-involved/get-quoted.php
@@ -0,0 +1,51 @@
+<!-- Meta Info -->
+<?php
+// Remove commented out marks before each line you wish to edit, and place content between quote marks.
+// $tpTitle="";
+// $pgDesc="";
+// $pgKeywords="";
+?>
+
+<!-- Header -->
+<?php include('../includes/header.php'); ?>
+
+	<!-- Begin Content -->
+	<section>
+		<div class="grid-wrap">								
+			<div class="grid-col bp3-col-3-4 pull-right-above-bp1">	
+
+				<!-- Main -->			
+				<article class="main">				
+					<h1 class="page-header">Get Quoted</h1>
+					<p class="featured-text">Join our bench of legal experts. Members of the media often contact John Marshall&rsquo;s Office of Public Affairs seeking an expert to comment on a current issue. These topics and areas of law range from intellectual property law to taxation, human rights to constitutional law.</p>
+					<p>Complete the form below to add your name to the list. (<em>Form coming.</em>)</p>					
+
+					<div class="grid-wrap">
+						<div class="grid-col col-3-4">
+						<div class="header-bar header-bar-gray">Form Name</div>
+							<div class="feature-box-color feature-box-gray">
+								<p>[Name]<br>
+								[Graduation Year]<br>
+								[Title]<br>
+								[Company]<br>
+								[Email]<br>
+								[Dropdown with Areas of Law] (List of areas coming)<br>
+								Recipient: ckraly@jmls.edu</p>
+							</div>
+						</div>
+					</div>
+				
+				</article><!-- .main -->
+				
+			</div><!-- .grid-col -->			
+						
+			<div class="grid-col bp3-col-1-4">
+				<!-- Sidebar -->
+				<?php include('../includes/sidebar.php'); ?>				
+			</div><!-- .grid-col -->			
+
+		</div><!-- .grid-wrap -->	
+	</section>
+
+<!-- Footer -->
+<?php include('../includes/footer.php'); ?>	
