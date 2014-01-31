0d404bc2b6ce8f89af13dacc2b3b0f93268dbb2f
Ryan Karpeles <rkarpel@jmls.edu>
2014-01-31 13:59:48 -0600

    Update links to images

--- includes/sidebar/connect.php
+++ includes/sidebar/connect.php
@@ -1,10 +1,20 @@
 <div class="module">
 	<div class="header-bar header-bar-gray">Connect Online</div>
 	<div class="social social-sidebar">
-		<div class="social-icon"><a href="https://www.facebook.com/JMLSAlumni" title="Facebook" target="_blank"><img class="social-icon-img-small" src="../images/icon-facebook-color.svg" alt="Facebook"></a></div>
-		<div class="social-icon"><a href="https://twitter.com/JMLSAlumni" title="Twitter" target="_blank"><img class="social-icon-img-small" src="../images/icon-twitter-color.svg" alt="Twitter"></a></div>
-		<div class="social-icon"><a href="http://www.linkedin.com/groups?gid=121368&trk=myg_ugrp_ovr" title="LinkedIn" target="_blank"><img class="social-icon-img-small" src="../images/icon-linkedin-color.svg" alt="LinkedIn"></a></div>
-		<div class="social-icon"><a href="http://www.youtube.com/jmlschicago" title="YouTube" target="_blank"><img class="social-icon-img-small" src="../images/icon-youtube-color.svg" alt="YouTube"></a></div>
-		<div class="social-icon"><a href="http://www.flickr.com/photos/43883549@N08/" title="Flickr" target="_blank"><img class="social-icon-img-small" src="../images/icon-flickr-color.svg" alt="Flickr"></a></div>
+		<div class="social-icon">
+			<a href="https://www.facebook.com/JMLSAlumni" title="Facebook" target="_blank"><img class="social-icon-img-small" src="http://localhost/alumni.jmls.edu/images/icon-facebook-color.svg" alt="Facebook"></a>
+		</div>
+		<div class="social-icon">
+			<a href="https://twitter.com/JMLSAlumni" title="Twitter" target="_blank"><img class="social-icon-img-small" src="http://localhost/alumni.jmls.edu/images/icon-twitter-color.svg" alt="Twitter"></a>
+		</div>
+		<div class="social-icon">
+			<a href="http://www.linkedin.com/groups?gid=121368&trk=myg_ugrp_ovr" title="LinkedIn" target="_blank"><img class="social-icon-img-small" src="http://localhost/alumni.jmls.edu/images/icon-linkedin-color.svg" alt="LinkedIn"></a>
+		</div>
+		<div class="social-icon">
+			<a href="http://www.youtube.com/jmlschicago" title="YouTube" target="_blank"><img class="social-icon-img-small" src="http://localhost/alumni.jmls.edu/images/icon-youtube-color.svg" alt="YouTube"></a>
+		</div>
+		<div class="social-icon">
+			<a href="http://www.flickr.com/photos/43883549@N08/" title="Flickr" target="_blank"><img class="social-icon-img-small" src="http://localhost/alumni.jmls.edu/images/icon-flickr-color.svg" alt="Flickr"></a>
+		</div>
 	</div>				
 </div>
