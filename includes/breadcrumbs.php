368f9a86a3e92cd8312326523adf6f2775f61e28
Ryan Karpeles <rkarpel@jmls.edu>
2014-02-06 12:06:07 -0600

    Update str_replace on labels

--- includes/breadcrumbs.php
+++ includes/breadcrumbs.php
@@ -1,18 +1,18 @@
 <?php
 	echo "<ul class='crumbs'>";
 	/* get array containing each directory name in the path */
-	$parts = explode("/", dirname($_SERVER['REQUEST_URI']));
+	$parts = explode("/", ($_SERVER['REQUEST_URI']));
 	echo "<li><a href='http://localhost/alumni.jmls.edu/''>Home</a></li>";
 	foreach ($parts as $key => $dir) {
 	switch ($dir) {
-	case "about": $label = "About Us"; break;
+	case "about": $label = "About Us"; break;	
 	/* if not in the exception list above,
 	use the directory name, capitalized */
-	default: $label = ucwords($dir); break;
+	default: $label = ucwords(str_replace(array(".php","and","-"),array("","&amp;"," "),$dir)); break;
 	}
 	/* start fresh, then add each directory back to the URL */
 	$url = "";
-	for ($i = 0; $i <= $key; $i++)
+	for ($i = 1; $i <= $key; $i++)
 	{ $url .= $parts[$i] . "/"; }
 	if ($dir != "")
 	echo "<li>> <a href='/$url'>$label</a></li>";
