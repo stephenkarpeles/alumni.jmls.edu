272a7a718194560ced5e68b6da0cf117312bcf75
Ryan Karpeles <rkarpel@jmls.edu>
2014-02-14 15:48:37 -0600

    Update giving links

--- support-john-marshall/giving-opportunities/index.php
+++ support-john-marshall/giving-opportunities/index.php
@@ -32,7 +32,7 @@
 					A memorial gift to The John Marshall law School is a thoughtful way to honor the memory of a friend, colleague or loved one, express sympathy, or provide a tribute for some other special occasion.</p>
 					<p><strong><a href="graduating-class-gift.php">Graduating Class Gift</a></strong><br>
 					Each graduating class has the opportunity to make a meaningful donation as a Class Gift to The John Marshall Law School.</p>
-					<p><strong><a href="#opp-6">Outright Gifts</a></strong><br>
+					<p><strong><a href="outright-gifts.php">Outright Gifts</a></strong><br>
 					The simplest, most common means of giving to The John Marshall Law School is to make a gift of cash, securities or personal property.</p>
 					<p><strong><a href="#opp-7">Make a Pledge</a></strong><br>
 					A pledge is an excellent demonstration of your committment to helping John Marshall advance its mission of providing access and opportunity. Your pledge allows you to choose what specific initiative you would like to support, for how long, and at what dollar amount.</p>
@@ -42,6 +42,8 @@
 					Strange as it may sound, a gift of &ldquo;startling significance&rdquo; often can be made at very low cost. It&rsquo;s just a matter of planning <em>what</em> to give, <em>how</em> to give and <em>when</em> to give.</p>
 					<p><strong><a href="#opp-10">Cy Pres Award</a></strong><br>
 					Cy Pres awards are residual funds in class action cases (and occasionally in other types of proceedings like bankruptcy and probate matters) that, for any number of reasons, are unclaimed or cannot be distributed to the class members or beneficiaries who were the intended recipients.</p>
+
+					<div class="mar-bot-larger"></div>
 					
 				</article><!-- .main -->
 				
---
+++ support-john-marshall/giving-opportunities/outright-gifts.php
@@ -0,0 +1,53 @@
+<!-- Meta Info -->
+<?php
+// Remove commented out marks before each line you wish to edit, and place content between quote marks.
+// $tpTitle="";
+// $pgDesc="";
+// $pgKeywords="";
+?>
+
+<!-- Header -->
+<?php include('../../includes/header.php'); ?>
+
+	<!-- Begin Content -->
+	<section>
+		<div class="grid-wrap">								
+			<div class="grid-col bp3-col-3-4 pull-right-above-bp1">	
+
+				<!-- Main -->			
+				<article class="main">
+
+				  <!-- Breadcrumbs -->		
+				  <?php include('../../includes/breadcrumbs.php'); ?>		
+				  		
+					<h1 class="page-header">Outright Gifts</h1>					
+					<p class="featured-text">The simplest, most common means of giving to The John Marshall Law School is to make a gift of cash, securities or personal property.</p> 
+					<p>These gifts can be put to work immediately and allow The John Marshall Law School maximum flexibility in continuing to strengthen our programs, improve our facilities and technology, and encourage outstanding scholarship from our faculty and students.</p>
+					<p>Your company or firm may offer a matching gift program. Matching gifts are an excellent way to double or even triple your contribution. For further information on matching gifts, please contact your personnel department or contact a Development Office staff member at 312.360.2663 or email <a href="mailto:development@jmls.edu"><strong>development@jmls.edu</strong></a>.</p>
+					<p>You can <a href="http://www.jmls.edu/give" target="_blank"><strong>make a cash gift securely online</strong></a> or send a check made payable to <strong>The John Marshall Law School Office of Development</strong> to:</p>
+					<p>The John Marshall Law School<br>
+					Office of Development<br>
+					315 S. Plymouth Ct.<br>
+					Chicago, IL 60604</p>		
+					<p>You are welcome to contact a Development Office staff member by calling 312.360.2663 or email <a href="mailto:development@jmls.edu"><strong>development@jmls.edu</strong></a>.</p>
+
+					<div class="mar-bot-medium"></div>
+					
+				</article><!-- .main -->
+				
+			</div><!-- .grid-col -->			
+						
+			<div class="grid-col bp3-col-1-4">
+				<!-- Sidebar Wrap Top -->
+				<?php include('../../includes/sidebar/sidebar-wrap-top.php'); ?>	
+				<!-- Menu -->
+				<?php include('../../includes/menus/support-john-marshall.php'); ?>
+				<!-- Sidebar Wrap Bottom -->
+				<?php include('../../includes/sidebar/sidebar-wrap-bottom-deans.php'); ?>					
+			</div><!-- .grid-col -->			
+
+		</div><!-- .grid-wrap -->	
+	</section>
+
+<!-- Footer -->
+<?php include('../../includes/footer.php'); ?>	
