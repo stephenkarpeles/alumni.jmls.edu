e9d3e9b4799b01a7017362095ca90823bd48cad9
Ryan Karpeles <rkarpel@jmls.edu>
2014-02-14 13:40:35 -0600

    Add Why Give Page

---
+++ support-john-marshall/why-give.php
@@ -0,0 +1,60 @@
+<!-- Meta Info -->
+<?php
+// Remove commented out marks before each line you wish to edit, and place content between quote marks.
+// $tpTitle="";
+// $pgDesc="";
+// $pgKeywords="";
+?>
+
+<!-- Header -->
+<?php include('../includes/header.php'); ?>
+
+	<!-- Begin Content -->
+	<section>
+		<div class="grid-wrap">								
+			<div class="grid-col bp3-col-3-4 pull-right-above-bp1">	
+
+				<!-- Main -->			
+				<article class="main">
+
+				  <!-- Breadcrumbs -->		
+				  <?php include('../includes/breadcrumbs.php'); ?>		
+				  		
+					<h1 class="page-header">Why Give</h1>
+					<p class="featured-text">Since its founding in 1899, The John Marshall Law School has provided the opportunity for over 16,000 individuals of every race, creed, gender, and background to achieve a legal education in the most dynamic urban setting in the world.</p>
+					<p>The Board of Trustees created the Development Office as an independent entity, dedicated solely to developing financial and community support for the law school, thus allowing the Dean to focus on the academic successes of the law school.</p>
+					<p>The Office of Development is committed to serving our donors with the highest level of integrity and appreciation, upholding the law school's core mission of preparing new attorneys grounded in the same fundamentals that have been taught here for more than 110 years.</p>
+
+					<hr>
+
+					<p>Your gift helps the law school and its students maximize potential by providing support in four areas:</p>
+					<p><strong>Student support</strong><br>
+					Academic and need-based scholarships, stipends, and bar exam preparation.</p>
+					<p><strong>Faculty support</strong><br>
+					Faculty recruitment and development, endowed chairs, and research and travel grants.</p>
+					<p><strong>Program support</strong><br>
+					Honors programs, clinics, externships, moot court and trial advocacy programs, and student life activities.</p>
+					<p><strong>Facilities and technology support</strong><br>
+					Library, computer services, upgraded classrooms, and capital improvements.</p>
+					
+						
+									
+
+				</article><!-- .main -->
+				
+			</div><!-- .grid-col -->			
+						
+			<div class="grid-col bp3-col-1-4">
+				<!-- Sidebar Wrap Top -->
+				<?php include('../includes/sidebar/sidebar-wrap-top.php'); ?>	
+				<!-- Menu -->
+				<?php include('../includes/menus/support-john-marshall.php'); ?>
+				<!-- Sidebar Wrap Bottom -->
+				<?php include('../includes/sidebar/sidebar-wrap-bottom-deans.php'); ?>					
+			</div><!-- .grid-col -->			
+
+		</div><!-- .grid-wrap -->	
+	</section>
+
+<!-- Footer -->
+<?php include('../includes/footer.php'); ?>	
