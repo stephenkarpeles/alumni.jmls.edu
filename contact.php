6e937f808ed25c321b23d684793e80e2644564b3
Ryan Karpeles <rkarpel@jmls.edu>
2014-02-18 13:59:10 -0600

    Update contact us page

--- contact.php
+++ contact.php
@@ -18,53 +18,48 @@ $tpTitle="Contact Us | The Office of Alumni Relations &amp; Development";
 					<h1 class="page-header">Contact Us</h1>
 					<p class="featured-text">Some content will go here introducing the contact section, and perhaps some of the people below, or what the best way is for people to get in touch.</p>
 
-					<h2 class="mar-bot-small">Alumni Relations Staff</h2>
+					<h2 class="mar-top-small mar-bot-small">Alumni Relations Staff</h2>
 
 					<div class="grid-wrap">
-						<div class="grid-col bp1-col-1-2 bp2-col-1-4 text-center">
+						<div class="grid-col bp1-col-1-2 bp2-col-1-4 mar-bot-medium text-center">
 							<img class="profile-thumb" src="http://placehold.it/145x198" alt="Sherri Dzik">
 							<div class="profile-name">Sherri Dzik</div>
 							<div class="profile-title">Director</div>
 							<a href="mailto:sdzikh@jmls.edu">sdzik@jmls.edu</a><br>
-							312.427.2737 ext. 343
-							</p>							
+							312.427.2737 ext. 343							
 						</div>
-						<div class="grid-col bp1-col-1-2 bp2-col-1-4 text-center">
+						<div class="grid-col bp1-col-1-2 bp2-col-1-4 mar-bot-medium text-center">
 							<img class="profile-thumb" src="http://placehold.it/145x198" alt="Matt Steward">
 							<div class="profile-name">Matt Steward</div>
 							<div class="profile-title">Assistant Director</div>
 							<a href="mailto:msteward@jmls.edu">msteward@jmls.edu</a><br>
 							312.427.2737 ext. 351
-							</p>
 						</div>
 					</div>
 
-					<h2 class="mar-bot-small">Development Staff</h2>
+					<h2 class="mar-top-small mar-bot-small">Development Staff</h2>
 
 					<div class="grid-wrap">
-						<div class="grid-col bp1-col-1-2 bp2-col-1-4 text-center">
+						<div class="grid-col bp1-col-1-2 bp2-col-1-4 mar-bot-medium text-center">
 							<img class="profile-thumb" src="images/veach-katherine.jpg" alt="Katherine Veach">
 							<div class="profile-name">Katherine Veach</div>
 							<div class="profile-title">Executive Director</div>
 							<a href="mailto:kveach@jmls.edu">kveach@jmls.edu</a><br>
-							312.427.2737 ext. 133
-							</p>							
+							312.427.2737 ext. 133							
 						</div>
-						<div class="grid-col bp1-col-1-2 bp2-col-1-4 text-center">
+						<div class="grid-col bp1-col-1-2 bp2-col-1-4 mar-bot-medium text-center">
 							<img class="profile-thumb" src="images/beach-bill.jpg" alt="Bill Beach">
 							<div class="profile-name">Bill Beach</div>
 							<div class="profile-title">Director, JMLS Fundraising Campaign</div>
 							<a href="mailto:bbeach@jmls.edu">bbeach@jmls.edu</a><br>
 							312.360.2663
-							</p>
 						</div>
-						<div class="grid-col bp1-col-1-2 bp2-col-1-4 text-center">
+						<div class="grid-col bp1-col-1-2 bp2-col-1-4 mar-bot-medium text-center">
 							<img class="profile-thumb" src="images/fagrelius-erik.jpg" alt="Erik Farelius">
 							<div class="profile-name">Erik Fagrelius</div>
 							<div class="profile-title">Director, Research, Grants &amp; Development Assessments</div>
 							<a href="mailto:efagrelius@jmls.edu">efagrelius@jmls.edu</a><br>
 							312.386.2869
-							</p>
 						</div>
 						<div class="grid-col bp1-col-1-2 bp2-col-1-4 text-center">
 							<img class="profile-thumb" src="images/melichar-ernie.jpg" alt="Ernie Melichar">
@@ -72,33 +67,32 @@ $tpTitle="Contact Us | The Office of Alumni Relations &amp; Development";
 							<div class="profile-title">Director, Planned Giving</div>
 							<a href="mailto:emelichar@jmls.edu">emelichar@jmls.edu</a><br>
 							312.427.2737 ext. 365
-							</p>
 						</div>
 					</div>
 
 					<div class="grid-wrap mar-top-medium">
-						<div class="grid-col bp1-col-1-2 bp2-col-1-4 text-center">
+						<div class="grid-col bp1-col-1-2 bp2-col-1-4 mar-bot-medium text-center">
 							<img class="profile-thumb" src="images/schaal-maridonna.jpg" alt="Maridonna Schaal">
 							<div class="profile-name">Maridonna Schaal</div>
 							<div class="profile-title">Director, Development Services</div>
 							<a href="mailto:mschaal@jmls.edu">mschaal@jmls.edu</a><br>
 							312.360.2663							
 						</div>
-						<div class="grid-col bp1-col-1-2 bp2-col-1-4 text-center">
+						<div class="grid-col bp1-col-1-2 bp2-col-1-4 mar-bot-medium text-center">
 							<img class="profile-thumb" src="images/hamill-judith.jpg" alt="Judith Hamill">
 							<div class="profile-name">Judith Hamill</div>
 							<div class="profile-title">Grants Administrator</div>
 							<a href="mailto:jhamill@jmls.edu">jhamill@jmls.edu</a><br>
 							312.427.2737 ext. 318
 						</div>
-						<div class="grid-col bp1-col-1-2 bp2-col-1-4 text-center">
+						<div class="grid-col bp1-col-1-2 bp2-col-1-4 mar-bot-medium text-center">
 							<img class="profile-thumb" src="images/prihoda-lauren.jpg" alt="Lauren Prihoda">
 							<div class="profile-name">Lauren Prihoda</div>
 							<div class="profile-title">Director, Annual Giving</div>
 							<a href="mailto:lprihoda@jmls.edu">lprihoda@jmls.edu</a><br>
 							312.386.2871
 						</div>
-						<div class="grid-col bp1-col-1-2 bp2-col-1-4 text-center">
+						<div class="grid-col bp1-col-1-2 bp2-col-1-4 mar-bot-medium text-center">
 							<img class="profile-thumb" src="images/beach-chris.jpg" alt="Chris Beach">
 							<div class="profile-name">Chris Beach</div>
 							<div class="profile-title">Research Analyst</div>
