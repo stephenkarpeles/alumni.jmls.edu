86e82cf6a74263644a35772d9e1d6147afb87d53
Ryan Karpeles <rkarpel@jmls.edu>
2014-02-27 14:24:42 -0600

    Update accordion style

--- support-john-marshall/faq.php
+++ support-john-marshall/faq.php
@@ -22,15 +22,18 @@
 				  		
 					<h1 class="page-header">Frequently Asked Questions</h1>
 
-					<div class="accordion-expand">Expand all</div>
-					<div class="accordion-collapse">Collapse all</div>
+					<div class="accordion-view">
+						<span class="accordion-expand">Show All Answers</span>
+						&nbsp;|&nbsp;
+						<span class="accordion-collapse">Hide All Answers</span>
+					</div>
 
 					<div class="accordion">
 
 						<!-- Accordion Item 1 -->
 					  <div class="accordion-header">
 					    <div class="accordion-number">1.</div>
-					    <div class="accordion-headline">How does the Development Office serve John Marshall?</div>					    
+					    <div class="accordion-header-text">How does the Development Office serve John Marshall?</div>
 					  </div>
 					  <div class="accordion-content">
 					    <p>The John Marshall Law School Office of Development exists solely for the benefit of the students, faculty, staff and alumni of John Marshall. The Development Office is the preferred channel for private gifts to all areas of the law school. The Development Office is governed by a board of directors that elects its officers and appoints its staff.</p>
@@ -41,14 +44,24 @@
 					  <!-- Accordion Item 2 -->					  
 					  <div class="accordion-header">
 					    <div class="accordion-number">2.</div>
-					    <div class="accordion-headline">What are the Development Office&rsquo;s fundraising priorities and who sets them?</div>
+					    <div class="accordion-header-text">What are the Development Office&rsquo;s fundraising priorities and who sets them?</div>
 					  </div>
 					  <div class="accordion-content">
 					    <p>Because The John Marshall Law School Office of Development is part of the law school and was established to facilitate the law school&rsquo;s mission, the Development Office&rsquo;s major initiatives, such as raising money for scholarships, facilities, and enhancing academic programs, are set by the Dean and the Board of Trustees of The John Marshall Law School.</p>    
 					  </div> 
 
+					  <!-- Accordion Item 3 -->					  
+					  <div class="accordion-header">
+					    <div class="accordion-number">3.</div>
+					    <div class="accordion-header-text">What is the difference between restricted and unrestricted gifts?</div>
+					  </div>
+					  <div class="accordion-content">
+					    <p>An unrestricted gift is given to the law school without any restrictions on its use&mdash;they enable John Marshall to place the contributions where the most critical need exists. Restricted gifts, on the other hand, are earmarked by the donor for a specific program or purpose.</p> 
+					    <p>Donations of any amount may be restricted to a specific program. Please note the program name when making your gift, or contact the Development Office for more information on restricting your gift.</p>  
+					  </div>
+					  
 					</div><!-- .accordion -->
-
+				
 				</article><!-- .main -->
 				
 			</div><!-- .grid-col -->			
