a8c13dcd35c7684908d4bcf85edc7d6cd2dbcd48
Ryan Karpeles <rkarpel@jmls.edu>
2014-02-27 14:40:47 -0600

    Update accordion section

--- js/site.js
+++ js/site.js
@@ -1,3 +1,4 @@
+// Modernizr fallback for switching out .svg with .png for earlier browsers
 if(!Modernizr.svg) {
   $('img[src*="svg"]').attr('src', function() {
     return $(this).attr('src').replace('.svg', '.png');
--- support-john-marshall/faq.php
+++ support-john-marshall/faq.php
@@ -59,6 +59,108 @@
 					    <p>An unrestricted gift is given to the law school without any restrictions on its use&mdash;they enable John Marshall to place the contributions where the most critical need exists. Restricted gifts, on the other hand, are earmarked by the donor for a specific program or purpose.</p> 
 					    <p>Donations of any amount may be restricted to a specific program. Please note the program name when making your gift, or contact the Development Office for more information on restricting your gift.</p>  
 					  </div>
+
+					  <!-- Accordion Item 4 -->					  
+					  <div class="accordion-header">
+					    <div class="accordion-number">4.</div>
+					    <div class="accordion-header-text">How does my gift go where the need is greatest?</div>
+					  </div>
+					  <div class="accordion-content">
+					    <p>The Board of Trustees and the Dean of The John Marshall Law School direct the use of unrestricted gifts based upon their direct knowledge of the law school&rsquo;s priorities.</p>  
+					  </div>
+
+					  <!-- Accordion Item 5 -->					  
+					  <div class="accordion-header">
+					    <div class="accordion-number">5.</div>
+					    <div class="accordion-header-text">What is the Annual Fund?</div>
+					  </div>
+					  <div class="accordion-content">
+					    <p>The Annual Fund provides essential unrestricted support for the Law School&rsquo;s highest priorities including scholarships, academic resources, and facilities. A gift to the Annual Fund, no matter the amount, will ensure that our students receive the best possible legal education.</p>  
+					  </div>
+
+					  <!-- Accordion Item 6 -->					  
+					  <div class="accordion-header">
+					    <div class="accordion-number">6.</div>
+					    <div class="accordion-header-text">What is an endowed fund?</div>
+					  </div>
+					  <div class="accordion-content">
+					    <p>An endowed fund is a sum of money invested to fund a particular project, such as a student scholarship. The initial investment, called the principal, is intended to continue forever, with the investment income from the principal providing perpetual funding. To endow a fund at The John Marshall Law School, a donor must agree to provide a principal of $25,000 and sign guidelines indicating how the earnings on the principal will be used.</p>  
+					  </div>
+
+					  <!-- Accordion Item 7 -->					  
+					  <div class="accordion-header">
+					    <div class="accordion-number">7.</div>
+					    <div class="accordion-header-text">Does the Development Office accept every gift offered by donors?</div>
+					  </div>
+					  <div class="accordion-content">
+					    <p>The John Marshall Law School Office of Development accepts most, but not all, gifts that are offered by donors. Cash gifts, and gifts which are readily or eventually convertible to cash (stocks, bonds, insurance policies, annuity contracts, etc.), are the most easily accepted. Gifts of real property (land, buildings, etc.) and tangible personal property (artwork, jewelry, historical artifacts, etc.) are accepted on a case-by-case basis after a thorough review of the following factors:</p>  
+					    <ul>
+					    	<li>Fair market value</li>
+								<li>Existence of restrictions</li>
+								<li>Reservations</li>
+								<li>Easements</li>
+								<li>Other limitations</li>
+								<li>Marketability of the property</li>
+								<li>Clear title</li>
+								<li>Location</li>
+								<li>Environmental assessment</li>
+								<li>Cost to John Marshall associated with ownership</li>
+								<li>Likelihood of use for John Marshall’s educational mission</li>
+					    </ul>
+					  </div>
+
+					  <!-- Accordion Item 8 -->					  
+					  <div class="accordion-header">
+					    <div class="accordion-number">8.</div>
+					    <div class="accordion-header-text">How do I make a gift?</div>
+					  </div>
+					  <div class="accordion-content">
+					    <p>You can make a gift by check, online, or over the phone.</p>
+							<p>You can make your check payable to The John Marshall Law School. (If you would like to support a specific program, please refer to it in the memo section of your check.)</p>
+
+							<p>Mail checks to:<br>
+							The John Marshall Law School Office of Development<br>
+							315 S. Plymouth Court<br>
+							Chicago, Illinois 60604</p>
+
+							<p>By phone: 312.386.2871</p>
+							<p>Online: <a class="bold" href="http://www.jmls.edu/give" target="_blank">Give to John Marshall Now</a></p>
+					  </div>
+
+					  <!-- Accordion Item 9 -->					  
+					  <div class="accordion-header">
+					    <div class="accordion-number">9.</div>
+					    <div class="accordion-header-text">Is my gift tax deductible?</div>
+					  </div>
+					  <div class="accordion-content">
+					    <p>Yes, to the extent allowable by law. You must claim the gift as an itemized deduction on your tax return.</p>
+					    <p>The John Marshall Law School is a nonprofit IRC Sec. 501(c)(3) organization.</p>  
+					  </div>
+
+					  <!-- Accordion Item 10 -->					  
+					  <div class="accordion-header">
+					    <div class="accordion-number">10.</div>
+					    <div class="accordion-header-text">Will I get a receipt for tax purposes?</div>
+					  </div>
+					  <div class="accordion-content">
+					    <p>Yes, soon after your gift is received, you will receive a printed receipt for your gift. If you give online, you&rsquo;ll also receive a confirmation of your gift via email.</p>  
+					  </div>
+
+					  <!-- Accordion Item 11 -->					  
+					  <div class="accordion-header">
+					    <div class="accordion-number">11.</div>
+					    <div class="accordion-header-text">I have more questions about making a gift. Whom do I contact?</div>
+					  </div>
+					  <div class="accordion-content">
+					    <p>If you have additional questions about making a gift, please contact us at:</p>  
+					    <p>The John Marshall Law School<br> 
+					    Office of Development<br>
+							315 S. Plymouth Court<br>
+							Chicago, Illinois 60604</p>
+
+							<p>Phone: 312.360.2663<br>
+							Email: <a class="bold" href="mailto:development@jmls.edu">development@jmls.edu</a></p>
+					  </div>
 					  
 					</div><!-- .accordion -->
 				
