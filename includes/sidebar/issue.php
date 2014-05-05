3cae8f070818db10bdd94e0d9dd7e502f49c55bc
Ryan Karpeles <rkarpel@jmls.edu>
2014-05-05 16:38:04 -0500

    Update sidebar and alumni news content

--- includes/sidebar/issue.php
+++ includes/sidebar/issue.php
@@ -2,7 +2,7 @@
 	<div class="header-bar header-bar-gold">Briefcase</div>
 	<div class="feature-box-color feature-box-gold text-small">
 		<div class="feature-box-col feature-box-col-1-2 text-small">		
-			Highlights alumni achievements and updates on JMLS development initiatives.
+			Highlights alumni achievements and updates to JMLS development initiatives.
 		</div>
 		<div class="feature-box-col feature-box-col-1-2 feature-box-col-raised text-center">	
 			<a href="#"><img class="mar-bot-smallest box-shadow-1" src="<?php echo $siteroot ?>/images/briefcase-cover.jpg" alt="Briefcase | The John Marshall Law School"></a>													
