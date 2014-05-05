8657e9287c9001a97293caa3562c4f4cd9c15d56
Ryan Karpeles <rkarpel@jmls.edu>
2014-05-05 16:31:22 -0500

    Update briefcase blurb and button language

--- includes/sidebar/issue.php
+++ includes/sidebar/issue.php
@@ -2,12 +2,12 @@
 	<div class="header-bar header-bar-gold">Briefcase</div>
 	<div class="feature-box-color feature-box-gold text-small">
 		<div class="feature-box-col feature-box-col-1-2 text-small">		
-			A blurb about the current issue and a short call to action perhaps.
+			Highlights alumni achievements and updates on JMLS development initiatives.
 		</div>
 		<div class="feature-box-col feature-box-col-1-2 feature-box-col-raised text-center">	
 			<a href="#"><img class="mar-bot-smallest box-shadow-1" src="<?php echo $siteroot ?>/images/briefcase-cover.jpg" alt="Briefcase | The John Marshall Law School"></a>													
 			<div>
-				<a href="#" class="btn btn-small btn-red bold">Read Issue</a>
+				<a href="#" class="btn btn-small btn-red bold">Read Now</a>
 			</div>
 		</div>						
 	</div>
