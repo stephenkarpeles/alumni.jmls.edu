c4348037ecf0aa2da00131c5e3274fc54cd82713
Ryan Karpeles <rkarpel@jmls.edu>
2014-05-07 16:42:37 -0500

    Update news feed RSS

--- includes/news-feed.php
+++ includes/news-feed.php
@@ -1,24 +1,13 @@
-<html>
-	<head>
-		<link rel="stylesheet" href="http://test.jmls.edu/testalumni/css/main.css" media="screen" type="text/css">
-		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,400italic,700italic" type="text/css">
-	</head>
-	<body>		
-
-		~~~BeginItemsRecord~~~
-						
-		<!-- News Item -->
-		<div class="media-box">
-			<div class="media">							
-				<div class="media-content">
-					<a class="bold" href="">~~~ItemTitle~~~</a>
-					<div class="subtext">~~~FeedPubLongDate~~~</div>
-				</div>
-			</div>
+~~~BeginItemsRecord~~~
+				
+<!-- News Item -->
+<div class="media-box">
+	<div class="media">							
+		<div class="media-content">
+			<a class="bold" href="">~~~ItemTitle~~~</a>
+			<div class="subtext">~~~FeedPubLongDate~~~</div>
 		</div>
+	</div>
+</div>
 
-		~~~EndItemsRecord~~~	
-
-	</body>
-
-</html>
+~~~EndItemsRecord~~~	
