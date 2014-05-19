dff4fa0678c5a6c79836741c1c96afca7d1212b2
Ryan Karpeles <rkarpel@jmls.edu>
2014-05-19 13:42:07 -0500

    Update News Page layout

--- stay-connected/alumni-news.php
+++ stay-connected/alumni-news.php
@@ -21,16 +21,26 @@ $tpTitle="Alumni News | The John Marshall Law School - Office of Alumni Relation
 				  
 					<h1 class="page-header">Alumni News</h1>
 					<p class="featured-text">Who&rsquo;s running for office? Who got promoted? Stay current on alumni news. <em>More description coming</em>.</p>
-					<h3>BriefCase</h3>
-					<p>Written specifically for John Marshall alumni, <em>BriefCase</em> highlights alumni accomplishments and offers updates on John Marshall&rsquo;s development initiatives. <a class="bold" href="http://news.jmls.edu/briefcase/">Read BriefCase Online</a>.</p>
-					<p><strong>Want to be in the next issue?</strong><br>
-					Send your new job, new baby, honor, recognition, or marriage news to <a href="mailto:alumni@jmls.edu" class="bold">alumni@jmls.edu</a>. Don&rsquo;t forget to include a picture!</p>
-					<h3>Alumni Newsletter</h3>
-					<p><em>Description coming</em></p>
-					<p>Not receiving the alumni newsletter? <a href="#" class="bold">Sign Up for Alumni News</a> <em>(Interface details coming)</em></p>
-					<h3>John Marshall News</h3>
-					<p><em>News feed from news.jmls.edu, ideally embedded like on the JMLS homepage with a link to the site for more headlines.</em></p>
-					<h3>Photo Gallery</h3>
+
+					<div class="grid-wrap">
+						<div class="grid-col bp3-col-1-3 mar-bot-medium">
+							<h3 class="mar-top-zero">BriefCase</h3>
+							<p>Written specifically for John Marshall alumni, <em>BriefCase</em> highlights alumni accomplishments and offers updates on John Marshall&rsquo;s development initiatives.<br><a class="bold" href="http://news.jmls.edu/briefcase/">Read BriefCase Online</a></p>
+							<p><strong>Want to be in the next issue?</strong><br>
+							Send your new job, new baby, honor, recognition, or marriage news to <a href="mailto:alumni@jmls.edu" class="bold">alumni@jmls.edu</a>. Don&rsquo;t forget to include a picture!</p>
+						</div>
+						<div class="grid-col bp3-col-1-3 mar-bot-medium">
+							<h3 class="mar-top-zero">Alumni Newsletter</h3>
+							<p><em>Description coming</em></p>
+							<p><strong>Not receiving the alumni newsletter?</strong><br><a href="#" class="bold">Sign Up for Alumni News</a> <em>(Interface details coming)</em></p>
+						</div>
+						<div class="grid-col bp3-col-1-3 mar-bot-medium">
+							<h3 class="mar-top-zero">John Marshall News</h3>
+							<p><em>News feed from news.jmls.edu, ideally embedded like on the JMLS homepage with a link to the site for more headlines.</em></p>
+						</div>
+					</div>					
+					
+					<h3 class="mar-top-zero">Photo Gallery</h3>
 					<p> <a href="https://www.flickr.com/photos/43883549@N08/sets/" target="_blank" class="bold">View Gallery on Flickr &raquo;</a></p>
 					<div class="grid-wrap">
 						<div class="grid-col col-1-2 bp2-col-1-4 mar-bot-medium">
