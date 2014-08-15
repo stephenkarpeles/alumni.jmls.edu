69f73c9456228899e9c823682059c122d74de4c0
Ryan Karpeles <rkarpel@jmls.edu>
2014-08-15 15:55:17 -0500

    Update Scholarships page with did you know facts

--- initiatives/clinical-experiences.php
+++ initiatives/clinical-experiences.php
@@ -22,18 +22,6 @@ $tpTitle="Transforming Lives Through Clinical Experiences | The Office of Alumni
 					<h1 class="page-header">Transforming Lives Through Clinical Experiences</h1>
 					<p class="featured-text">Help support the experience-based training John Marshall students need to be practice ready from day one. By supporting our clinical programs, you can be part of the $3.5 million dollar pro-bono impact our students have on their community.</p>
 					<p>John Marshall is the first school in the Midwest, and one of the few in the nation, to require clinical experience for graduation. This requirement builds upon the wide range of skills students learn in our #2-ranked Lawyering Skills Program.</p>
-
-					<div class="module callout-box">
-						<div class="header-bar header-bar-red">Did You Know?</div>
-						<div class="feature-box-color feature-box-gray">
-						  <div class="position-front">
-						    <span class="feat-arrow">&#9654;</span>
-								<p class="feat-arrow-content">John Marshall serves over <span class="red serif gamma">3,568</span> veterans every year through the Veterans Legal Support Clinic.</p>
-							</div>
-							<div class="icon-question position-middle"></div>
-						</div>
-					</div>
-
 					<p>Clinical and externship experiences hone students' abilities to perform essential lawyering skills prior to graduation, offer them an assessment of necessary competencies, and help to ensure that our students graduate with the knowledge, skills, and experience employers demand most.</p>
 					<p>These practical experiences have a profound effect on the student attorney as they deepen their understanding of the relationship between the law school doctrine and real-world client environments. Students learn in our clinics, while becoming advocates in our community.</p>
 					<p>To earn the required credit hours, students choose from eight clinical opportunities, including: Business Enterprise Law Clinic, Domestic Violence Clinic, Fair Housing Legal Clinic, Institute for Conflict Resolution, International Human Rights Clinic, IP Patent Clinic, Pro Bono Clinic, and Veterans Legal Support Clinic.</p>
--- initiatives/scholarships.php
+++ initiatives/scholarships.php
@@ -21,8 +21,32 @@ $tpTitle="Scholarships Transforms Lives | The Office of Alumni Relations &amp; D
 
 					<h1 class="page-header">Scholarships Transforms Lives</h1>
 					<p class="featured-text">Scholarships are the number one reason a student chooses a law school. But the rising cost of law school is making it harder for our students&mdash;many of whom are already working full- or part-time&mdash;to continue their studies. To remain competitive and attract academically distinguished students, we need to increase the number and amount of our scholarships.</p>
-					<p>Of the 85% of our applicants who have financial needs, only 60% of our entering class is awarded scholarships, and the median amount awarded this year was $12,000.  For John Marshall to continue to offer an outstanding legal education to deserving students from our community, we need to increase the number and amount of our scholarships.</p>
+
+					<div class="module callout-box">
+						<div class="header-bar header-bar-red">Did You Know?</div>
+						<div class="feature-box-color feature-box-gray">
+						  <div class="position-front">
+						    <span class="feat-arrow">&#9654;</span>
+								<p class="feat-arrow-content"><span class="red serif gamma">85%</span> of John Marshall applicants have financial needs.</p>
+							</div>
+							<div class="icon-question position-middle"></div>
+						</div>
+					</div>
+
+					<p>For John Marshall to continue to offer an outstanding legal education to deserving students from our community, we need to increase the number and amount of our scholarships.</p>
 					<p>Borrowing to pay for law school limits our graduates' career paths to those with salaries sufficient to pay off student loans. As a result, graduates shy away from public interest-sector positions, leaving a gaping hole in a practice area that provides a valuable service to our communities. In order for our graduates to realize their dreams without the straitjacket of loan repayment, we need to increase the number and amount of our scholarships.</p>
+
+					<div class="module callout-box">
+						<div class="header-bar header-bar-red">Did You Know?</div>
+						<div class="feature-box-color feature-box-gray">
+						  <div class="position-front">
+						    <span class="feat-arrow">&#9654;</span>
+								<p class="feat-arrow-content">Only <span class="red serif gamma">60%</span> of our entering class is awarded scholarships.</p>
+							</div>
+							<div class="icon-question position-middle"></div>
+						</div>
+					</div>
+
 					<p>Scholarship support also helps our students have time for honors programs, student organizations, and giving back to the community through pro bono activities that can be very difficult or impossible to engage in while working long hours during law school. In order for our students to distinguish themselves and draw on the full spectrum of law school offerings, we need to increase the number and amount of our scholarships.</p>
 					<p>Along with scholarship support, other student-related investments, such as internships and bar preparation courses enable you to have a direct and transformational impact on the lives of our students.</p>
 
