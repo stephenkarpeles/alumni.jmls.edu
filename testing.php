16afebe528c275293a584725a4f590482d3d7cb5
Ryan Karpeles <rkarpel@jmls.edu>
2014-08-21 10:30:29 -0500

    Update login links and .inc files

---
+++ testing.php
@@ -0,0 +1,103 @@
+<!-- Meta content goes below. Write content between quote marks. -->
+<?php
+$tpTitle="Testing | The Office of Alumni Relations &amp; Development";
+// $pgDesc="";
+// $pgKeywords="";
+?>
+
+<!-- Header -->
+<?php include('includes/header.php'); ?>
+
+	<!-- Begin Content -->
+	<section>
+		<div class="grid-wrap">								
+			<div class="grid-col bp3-col-3-4 pull-right-above-bp1">	
+
+				<!-- Main -->			
+				<article class="main">	
+
+				  <!-- Breadcrumbs -->		
+				  <?php include('includes/breadcrumbs.php'); ?>
+
+					<h1 class="page-header">Testing page</h1>
+					
+					<script src="http://max.jotfor.ms/static/prototype.forms.js" type="text/javascript"></script>
+						<script src="http://max.jotfor.ms/static/jotform.forms.js?3.2.3296" type="text/javascript"></script>
+						<script type="text/javascript">
+						   JotForm.init();
+						</script>
+						<link href="http://max.jotfor.ms/static/formCss.css?3.2.3296" rel="stylesheet" type="text/css" />
+						<link type="text/css" rel="stylesheet" href="http://max.jotfor.ms/css/styles/nova.css?3.2.3296" />
+						<link type="text/css" media="print" rel="stylesheet" href="http://max.jotfor.ms/css/printForm.css?3.2.3296" />
+						<style type="text/css">
+						    .form-label-left{
+						        width:150px !important;
+						    }
+						    .form-line{
+						        padding-top:12px;
+						        padding-bottom:12px;
+						    }
+						    .form-label-right{
+						        width:150px !important;
+						    }
+						    .form-all{
+						        width:690px;
+						        color:#555 !important;
+						        font-family:'Lucida Grande';
+						        font-size:14px;
+						    }
+						</style>
+
+						<form class="jotform-form" action="http://submit.jotform.us/submit/42315108779155/" method="post" name="form_42315108779155" id="42315108779155" accept-charset="utf-8">
+						  <input type="hidden" name="formID" value="42315108779155" />
+						  <div class="form-all">
+						    <ul class="form-section">
+						      <li class="form-line" data-type="control_textbox" id="id_1">
+						        <label class="form-label form-label-left form-label-auto" id="label_1" for="input_1"> Name </label>
+						        <div id="cid_1" class="form-input">
+						          <input type="text" class=" form-textbox" data-type="input-textbox" id="input_1" name="q1_name" size="20" value="" />
+						        </div>
+						      </li>
+						      <li class="form-line" data-type="control_textbox" id="id_4">
+						        <label class="form-label form-label-left form-label-auto" id="label_4" for="input_4"> Test box </label>
+						        <div id="cid_4" class="form-input">
+						          <input type="text" class=" form-textbox" data-type="input-textbox" id="input_4" name="q4_testBox" size="20" value="" />
+						        </div>
+						      </li>
+						      <li class="form-line" data-type="control_button" id="id_2">
+						        <div id="cid_2" class="form-input-wide">
+						          <div style="margin-left:156px" class="form-buttons-wrapper">
+						            <button id="input_2" type="submit" class="form-submit-button">
+						              Submit
+						            </button>
+						          </div>
+						        </div>
+						      </li>
+						      <li style="display:none">
+						        Should be Empty:
+						        <input type="text" name="website" value="" />
+						      </li>
+						    </ul>
+						  </div>
+						  <input type="hidden" id="simple_spc" name="simple_spc" value="42315108779155" />
+						  <script type="text/javascript">
+						  document.getElementById("si" + "mple" + "_spc").value = "42315108779155-42315108779155";
+						  </script>
+						</form>
+
+				</article><!-- .main -->
+
+			</div><!-- .grid-col -->			
+						
+			<div class="grid-col bp3-col-1-4">
+				<!-- Sidebar Wrap Top -->
+				<?php include('includes/sidebar/sidebar-wrap-top.php'); ?>
+				<!-- Sidebar Wrap Bottom -->
+				<?php include('includes/sidebar/sidebar-wrap-bottom-alt.php'); ?>					
+			</div><!-- .grid-col -->						
+
+		</div><!-- .grid-wrap -->	
+	</section>
+
+<!-- Footer -->
+<?php include('includes/footer.php'); ?>
