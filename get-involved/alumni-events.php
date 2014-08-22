82386c0ef0febce08540f8d27cb3d26583fa984b
Ryan Karpeles <rkarpel@jmls.edu>
2014-08-22 13:05:37 -0500

    Updated design on Alumni Events page

--- get-involved/alumni-events.php
+++ get-involved/alumni-events.php
@@ -21,16 +21,47 @@ $tpTitle="Alumni Events | The John Marshall Law School - Office of Alumni Relati
 				  			
 					<h1 class="page-header">Alumni Events</h1>
 					<p class="featured-text">Throughout the year, the Office of Alumni Relations hosts events tailored specifically to the professional needs of our alumni.</p>
+
+					<div class="module callout-box">
+						<div class="header-bar header-bar-gray">Upcoming Events</div>
+						<div class="feature-box-color feature-box-gray">
+							<p><a href="http://events.jmls.edu/registration/node/602"><strong>Young Alumni Council: Eggs &amp; Issues</strong></a><br>
+							August 28, 2014
+							</p>
+							<p ><a href="http://events.jmls.edu/registration/node/604"><strong >11th Annual Student/Alumni Exchange</strong></a><br>
+							September 11, 2014
+							</p>
+							<p><a href="http://events.jmls.edu/registration/node/587"><strong>Supreme Court Year In Review</strong></a><br>
+							September 19, 2014
+							</p>
+							<p><a href="http://events.jmls.edu/registration/node/556"><strong>2014 JMLS Reunion Weekend</strong></a><br>
+							September 19&ndash;20, 2014
+							</p>
+							<p><a href="http://events.jmls.edu/registration/node/605"><strong>Naperville Alumni Reception</strong></a><br>
+							October 9&ndash;20, 2014
+							</p>
+						</div>
+					</div>
+
 					<h3>CLE Programs</h3>
 					<p>As a service to John Marshall alumni and the legal community, we offer high-quality, affordable CLE and professional development programs. Whether you are brushing up on a practice area or want to learn about something new, you&rsquo;ll be able to meet your CLE requirements with ease. </p>
 					<p><a class="bold" href="http://events.jmls.edu/registration/taxonomy/term/17" target="_blank">View a Full List of John Marshall CLE Events</a></p>
 					<p><em>Hardship waivers for more recent alumni are available for many CLE programs. Please contact Cliff Scott-Rudnick for fee adjustment requests due to financial hardship at <a href="mailto:9scott@jmls.edu" class="bold">9scott@jmls.edu</a>.</em></p>
+
+					<div class="module callout-box mar-top-medium">
+					  <div class="header-bar header-bar-red">Alumni Reunions</div>
+            <div class="feature-box-color feature-box-gray">
+							<span class="feat-arrow up-1">&#9654;</span><span class="feat-arrow-content"><strong><a href="http://www.jmls.edu/alumni-reunion/2014/">2014 JMLS Reunion Weekend</a></strong><br>September 19&ndash;20, 2014</span>
+						</div>
+					</div>
+
 					<h3>Alumni Networking Events &amp; Receptions</h3>
 					<p>Attending alumni receptions allows you a chance to socialize, reminisce, network, and hear about what is going on at your alma mater. Receptions are held throughout the Chicago area and occasionally in various cities around the country.</p>
 					<p>Check back often as we will update this page as events are added and dates are confirmed. As always, make sure that we have your current contact information so that you will receive your invitation.</p>
+					
 
 					<h3>Photo Gallery</h3>
-					<p> <a href="https://www.flickr.com/photos/43883549@N08/sets/" target="_blank" class="bold">View Gallery on Flickr &raquo;</a></p>
+					<p><a href="https://www.flickr.com/photos/43883549@N08/sets/" target="_blank" class="bold">View Gallery on Flickr &raquo;</a></p>
 					<div class="grid-wrap">
 						<div class="grid-col col-1-2 bp2-col-1-4 mar-bot-medium">
 							<img src="../images/gallery-1.jpg" alt="">
